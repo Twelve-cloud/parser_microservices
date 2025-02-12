b9a3dea2b2a2f74e50d8c377ad43e416ecbaf995
= <kana.suzucki@gmail.com>
2025-02-13 01:03:58 +0300

    add volumes to postgres, mongo and redis

--- infrastructure/twich_parser_service/compose/entrypoints/postgres/entrypoint.sh
+++ infrastructure/twich_parser_service/compose/entrypoints/postgres/entrypoint.sh
@@ -6,5 +6,8 @@ set -e
 # Running postgres entrypoint
 echo "Running postgres entrypoint"
 
+# Giving permission for postgres data folder
+chmod -R 0700 ${PGDATA}
+
 # Run original entrypoint
 exec /usr/local/bin/docker-entrypoint.sh "$@"
