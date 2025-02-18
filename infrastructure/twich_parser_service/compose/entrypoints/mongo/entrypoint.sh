278e9f606516c4576904e6e30269ad6c26289480
= <kana.suzucki@gmail.com>
2025-02-19 00:18:03 +0300

    add Dockerfile for mongo, postgres, redis

--- infrastructure/twich_parser_service/compose/entrypoints/mongo/entrypoint.sh
+++ infrastructure/twich_parser_service/compose/entrypoints/mongo/entrypoint.sh
@@ -6,14 +6,8 @@ set -e
 # Running mongo entrypoint
 echo "Running mongo entrypoint"
 
-# Copy mongo config file from template
-cp /etc/templates/mongo/mongo.conf.template /etc/mongo/mongo.conf
-
 # Substitute template variables
-sed -i                                                                         \
-    -e "s|\${MONGO_HOST}|${MONGO_HOST}|g"                                      \
-    -e "s|\${MONGO_PORT}|${MONGO_PORT}|g"                                      \
-    /etc/mongo/mongo.conf
+envsubst < /etc/templates/mongo/mongo.conf.template > /etc/mongo/mongo.conf
 
 # Run original entrypoint
 exec /usr/local/bin/docker-entrypoint.sh "$@"
