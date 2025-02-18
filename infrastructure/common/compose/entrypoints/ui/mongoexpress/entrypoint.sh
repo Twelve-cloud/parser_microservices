98e3cc46bde289fdfc8caf529c021838f57893f2
= <kana.suzucki@gmail.com>
2025-02-19 01:07:44 +0300

    add env support for mongo express

--- infrastructure/common/compose/entrypoints/ui/mongoexpress/entrypoint.sh
+++ infrastructure/common/compose/entrypoints/ui/mongoexpress/entrypoint.sh
@@ -6,5 +6,9 @@ set -e
 # Running mongo-express entrypoint
 echo "Running mongo-express entrypoint"
 
+# Substitute template variables
+envsubst < /etc/templates/mongoexpress/connect.js.template > /app/lib/db.js
+envsubst < /etc/templates/mongoexpress/mongoexpress.js.template > /app/config.default.js
+
 # Run original entrypoint
 exec /docker-entrypoint.sh "$@"
