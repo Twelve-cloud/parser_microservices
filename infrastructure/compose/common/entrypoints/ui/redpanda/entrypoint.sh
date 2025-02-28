703ca3dc9a2106e2f1e04bad2cc8e250a3f3d666
= <kana.suzucki@gmail.com>
2025-02-28 12:23:07 +0300

    add some config for redpanda

---
+++ infrastructure/compose/common/entrypoints/ui/redpanda/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Exit if any command here fails
+set -e
+
+# Running redpanda entrypoint
+echo "Running redpanda entrypoint"
+
+# Substitute template variables
+envsubst < ${_REDPANDA_CONFIG_TEMPLATE_PATH} > ${_REDPANDA_CONFIG_PATH}
+
+# Run redpanda
+exec ./console
