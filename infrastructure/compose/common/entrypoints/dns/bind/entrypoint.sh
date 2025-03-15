0d9e68c2a5bb224208bd1938b428b7d546dc7861
= <kana.suzucki@gmail.com>
2025-03-15 12:35:57 +0300

    bind

--- infrastructure/compose/common/entrypoints/dns/bind/entrypoint.sh
+++ infrastructure/compose/common/entrypoints/dns/bind/entrypoint.sh
@@ -9,5 +9,9 @@ echo "Running bind entrypoint"
 # Substitute template variables
 envsubst < ${_BIND_CONFIG_TEMPLATE_PATH} > ${_BIND_CONFIG_PATH}
 
+# Change all
+cp /etc/templates/bind/zones/db.par     /var/lib/bind/db.par
+cp /etc/templates/bind/zones/db.ui.par  /var/lib/bind/db.ui.par
+
 # Run original entrypoint
 exec ${_BIND_ORIGINAL_ENTRYPOINT_PATH} "$@"
