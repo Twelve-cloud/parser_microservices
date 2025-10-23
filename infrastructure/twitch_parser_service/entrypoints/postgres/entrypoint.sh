c1acca80bf4ecd5a1354224d846de30cca8f4776
= <kana.suzucki@gmail.com>
2025-10-24 00:32:13 +0300

    fix: remove postgres scram-sha-256 for auth (only cert auth), fix some errors and warnings

--- infrastructure/twitch_parser_service/entrypoints/postgres/entrypoint.sh
+++ infrastructure/twitch_parser_service/entrypoints/postgres/entrypoint.sh
@@ -6,9 +6,6 @@ set -e
 # Running postgres entrypoint
 echo "Running postgres entrypoint"
 
-# Giving permission for postgres data folder
-chmod -R 0700 ${PGDATA}
-
 # Substitute template variables
 envsubst < ${_POSTGRES_CONFIG_TEMPLATE_PATH} > ${_POSTGRES_CONFIG_PATH}
 envsubst < ${_POSTGRES_HBA_TEMPLATE_PATH} > ${_POSTGRES_HBA_PATH}
