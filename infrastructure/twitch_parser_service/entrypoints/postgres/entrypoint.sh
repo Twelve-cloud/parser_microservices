33123b83eb634b050fa9cbd1596933ec003781ed
= <kana.suzucki@gmail.com>
2025-12-07 21:05:21 +0300

    style: delete all comments in postgresql entrypoint (they are useless)

--- infrastructure/twitch_parser_service/entrypoints/postgres/entrypoint.sh
+++ infrastructure/twitch_parser_service/entrypoints/postgres/entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+
 # Exit if any command here fails
 set -e
 
-# Function to log message
+
 log_message() {
     # Get message
     local message="$1"
@@ -18,11 +19,10 @@ log_message() {
     echo "[${timestamp}] [PID ${pid}] LOG: ${message}"
 }
 
-# Function to initialize database
+
 initialize_database() {
     log_message "Initializing database (running initdb)"
 
-    # Initialize database (create system databases, etc...)
     pg_ctl init                                                                                             \
         --silent                                                                                            \
         --pgdata "${_ENTRYPOINT_POSTGRES_PGDATA}"                                                           \
@@ -34,11 +34,10 @@ initialize_database() {
     log_message "Database has been initialized (initdb complete)"
 }
 
-# Function to start temporary server
+
 start_temporary_server() {
     log_message "Starting temporary server (pg_ctl start)"
 
-    # Start temporary server
     pg_ctl start                                                                                            \
         --silent                                                                                            \
         --wait                                                                                              \
@@ -50,18 +49,14 @@ start_temporary_server() {
     log_message "Temporary server has been started (pg_ctl start)"
 }
 
-# Function to apply init files
+
 apply_init_files() {
     log_message "Applying init files"
 
-    # Create variable to store path to init files
     local init_file
 
-    # Loop through init files
     for init_file in "${@}"; do
-        # Check if init file is SQL
         case "${init_file}" in
-            # If init file is SQL run it
             *.sql) log_message "Running SQL file: ${init_file}"; run_sql -f "${init_file}" ;;
         esac
     done
@@ -69,9 +64,8 @@ apply_init_files() {
     log_message "Init files have been applied"
 }
 
-# Function to run SQL
+
 run_sql() {
-    # Create variable to store psql connection parameters
     local query_runner=(
         psql
         --variable ON_ERROR_STOP=1
@@ -89,15 +83,13 @@ run_sql() {
         "
     )
 
-    # Run SQL
     "${query_runner[@]}" "$@"
 }
 
-# Function to stop temporary server
+
 stop_temporary_server() {
     log_message "Stopping temporary server (pg_ctl stop)"
 
-    # Stop temporary server
     pg_ctl stop                                                                                             \
         --silent                                                                                            \
         --wait                                                                                              \
@@ -107,13 +99,12 @@ stop_temporary_server() {
     log_message "Temporary server has been stopped (pg_ctl stop)"
 }
 
-# Function to run main
+
 main() {
     log_message "Running postgres entrypoint"
 
     log_message "Substituting template variables"
 
-    # Substitute template variables
     envsubst < "${_ENTRYPOINT_POSTGRES_CONFIG_TEMPLATE_PATH}" > "${_ENTRYPOINT_POSTGRES_CONFIG_PATH}"
     envsubst < "${_ENTRYPOINT_POSTGRES_HBA_TEMPLATE_PATH}" > "${_ENTRYPOINT_POSTGRES_HBA_PATH}"
     envsubst < "${_ENTRYPOINT_POSTGRES_IDENT_TEMPLATE_PATH}" > "${_ENTRYPOINT_POSTGRES_IDENT_PATH}"
@@ -129,16 +120,12 @@ main() {
 
     # If database is not initialized (file PG_VERSION does not exist or empty)
     if [[ ! -s "${_ENTRYPOINT_POSTGRES_PGDATA}/PG_VERSION" ]]; then
-        # Initialize database
         initialize_database
 
-        # Start temporary server
         start_temporary_server
 
-        # Apply init files
         apply_init_files "${_ENTRYPOINT_POSTGRES_INIT_SQL_PATH}"
 
-        # Stop temporary server
         stop_temporary_server
 
         log_message "PostgreSQL init process complete; Ready for start up"
@@ -146,9 +133,8 @@ main() {
         log_message "PostgreSQL Database directory contain a database; Skipping initialization"
     fi
 
-    # Run postgres
     exec "${@}"
 }
 
-# Run main
+
 main "${@}"
